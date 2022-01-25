fa7b1f512d4d8d5326d152ca92e82b87eae50786
Usatoshi <66457284+Usatoshi@users.noreply.github.com>
2022-01-25 17:03:38 +0900

    Ball転がす処理とテクスチャ追加（Test）

---
+++ Assets/Member/ookubo/Ball/Script/RollOver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Ballを転がす処理
+/// </summary>
+public class RollOver : MonoBehaviour
+{
+    private Rigidbody rb;   //  重力
+    [SerializeField] private Vector3 force; // 力を加える
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Ballの重力値を取得
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  力を加える
+        rb.AddForce(force);
+
+        
+    }
+}
