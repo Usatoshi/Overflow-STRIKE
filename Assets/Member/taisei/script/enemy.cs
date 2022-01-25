80770c021192af6456b036250cb0fe496fa3eff9
Usatoshi <66457284+Usatoshi@users.noreply.github.com>
2022-01-25 18:13:15 +0900

    的Prefab作成

--- Assets/Member/taisei/script/enemy.cs
+++ Assets/Member/taisei/script/enemy.cs
@@ -5,27 +5,28 @@ using UnityEngine;
 public class enemy : MonoBehaviour
 {
     //敵プレハブ
-    public GameObject enemyPrefab;
-    //時間間隔の最小値
-    public float minTime = 2f;
-    //時間間隔の最大値
-    public float maxTime = 5f;
-    //X座標の最小値
-    public float xMinPosition = -10f;
-    //X座標の最大値
-    public float xMaxPosition = 10f;
-    //Y座標の最小値
-    public float yMinPosition = 0f;
-    //Y座標の最大値
-    public float yMaxPosition = 10f;
-    //Z座標の最小値
-    public float zMinPosition = 10f;
-    //Z座標の最大値
-    public float zMaxPosition = 20f;
+    [SerializeField] private GameObject enemyPrefab;
+
+    /// <summary>
+    /// 時間系
+    /// </summary>
+    //  最小値
+    [SerializeField] private float minTime = 2f;
+    //  最大値
+    [SerializeField] private float maxTime = 5f;
+
+    /// <summary>
+    /// ランダム生成（位置）
+    /// </summary>
+    //  最小値
+    [SerializeField] private Vector3 MinPosition;
+    //  最大値
+    [SerializeField] private Vector3 MaxPosition;
+
     //敵生成時間間隔
     private float interval;
     //経過時間
-    private float time = 0f;
+    private float nowTime = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -38,17 +39,17 @@ public class enemy : MonoBehaviour
     void Update()
     {
         //時間計測
-        time += Time.deltaTime;
+        nowTime += Time.deltaTime;
 
         //経過時間が生成時間になったとき(生成時間より大きくなったとき)
-        if (time > interval)
+        if (nowTime > interval)
         {
             //enemyをインスタンス化する(生成する)
             GameObject enemy = Instantiate(enemyPrefab);
             //生成した敵の位置をランダムに設定する
             enemy.transform.position = GetRandomPosition();
             //経過時間を初期化して再度時間計測を始める
-            time = 0f;
+            nowTime = 0f;
             //次に発生する時間間隔を決定する
             interval = GetRandomTime();
         }
@@ -64,9 +65,10 @@ public class enemy : MonoBehaviour
     private Vector3 GetRandomPosition()
     {
         //それぞれの座標をランダムに生成する
-        float x = Random.Range(xMinPosition, xMaxPosition);
-        float y = Random.Range(yMinPosition, yMaxPosition);
-        float z = Random.Range(zMinPosition, zMaxPosition);
+        float x = Random.Range(MinPosition.x, MaxPosition.x);
+        float y = Random.Range(MinPosition.y, MaxPosition.y);
+        float z = Random.Range(MinPosition.z, MaxPosition.z);
+
 
         //Vector3型のPositionを返す
         return new Vector3(x, y, z);
